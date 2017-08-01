b596a85d802dd3f255ab5cd0eb3f8d16dfb35076
JhonJRG <1060808509@qq.com>
2017-08-02 02:28:28 +0800

    Update router.js

---
+++ 跨域/CORS/router.js
@@ -0,0 +1,22 @@
+router.get('/getNews',function(req,res){
+  var news = [
+    "1.这是第1行",
+    "2.这是第2行",
+    "3.这是第3行",
+    "4.这是第4行", 
+    "5.这是第5行",
+    "6.这是第6行",
+    "7.这是第7行",
+    "8.这是第8行"
+  ]
+  var data = [];
+  for(var i=0; i<3; i++){
+    var index = parseInt(Math.random()*news.length);
+    data.push(news[index]);
+    news.splice(index,1);
+  }
+  res.header("Access-Control-Allow-Origin","http://a.jrg.com:8080");
+  // res.header("Access-Control-Allow-Origin","*");
+  res.send(data);
+
+})
