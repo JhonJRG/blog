73c90fe550df1e9e93837cfa97550ad959652c2f
JhonJRG <1060808509@qq.com>
2017-08-09 03:12:31 +0800

    Update router.js

--- 跨域/postMessage/router.js
+++ 跨域/postMessage/router.js
@@ -1,5 +1,5 @@
-// 访问之前先：
-//   在host文件（C:\Windows\System32\drivers\etc）中加入以下语句，即可通过 a.jrg.com:8080/xxxx.html 访问
+//   提示：
+//   访问之前先：在host文件（C:\Windows\System32\drivers\etc）中加入以下语句，即可通过 a.jrg.com:8080/xxxx.html 访问
 //   127.0.0.1       a.jrg.com
 //   127.0.0.1       b.jrg.com
 //   127.0.0.1       jrg.com 
